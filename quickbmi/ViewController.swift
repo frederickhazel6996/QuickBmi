bd2cbaaf2545cd506f0d18a6926c21198be8a8eb
Frederick Hazel <frederick.d9696@gmail.com>
2020-12-22 20:15:52 +0000

    learning auto layout

--- quickbmi/ViewController.swift
+++ quickbmi/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageLabel: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
-
+    @IBAction func changeAge(_ sender: UIButton) {
+        let sign=sender.currentTitle
+        var age=Int(ageLabel.text!)
+    
+        if sign=="-"{
+            
+            if(age!==0){
+                print(0)
+                ageLabel.text=String(0)
+            }
+            else{
+                
+                var newage=age!-1
+                print(newage)
+                ageLabel.text=String(newage)
+            }
+            
+            
+            
+            
+            
+        }
+        if sign=="+"{
+            var newage=age!+1
+            print(newage)
+            ageLabel.text=String(newage)
+        }
+    }
 }
 
